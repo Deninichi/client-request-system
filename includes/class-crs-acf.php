416418af3b2ba660a56fd887a0330b71b51403a5
Denis <deninichi@gmail.com>
2018-06-12 02:07:13 +0400

    ACF Class

---
+++ includes/class-crs-acf.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The file that defines the ACF class
+ *
+ * @link       http://deninichi.com
+ * @since      1.0.0
+ *
+ * @package    CRS
+ * @subpackage CRS/includes
+ */
+
+/**
+ * The ACF plugin class.
+ *
+ * @since      1.0.0
+ * @package    CRS
+ * @subpackage CRS/includes
+ * @author     Denis Nichik <crs@deninichi.com>
+ */
+class CRS_ACF {
+
+}
